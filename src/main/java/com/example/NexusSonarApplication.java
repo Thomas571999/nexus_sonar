97d9e3e6650de91a41d0b378421a81540c188475
Thomas571999 <thomas.goncalves571999@gmail.com>
2026-02-05 11:49:15 +0100

    Modif pom.xml

---
+++ src/main/java/com/example/NexusSonarApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusSonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusSonarApplication.class, args);
+    }
+
+}
